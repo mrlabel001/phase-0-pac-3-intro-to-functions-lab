beba1d85157404d1d02ce71646353635d42c8020
mrlabel <farbean.makini@student.moringaschool.com>
2023-09-19 11:11:35 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {}
+function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase();
+  
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function logWhisper(string) {
+    console.log(string.toLowerCase( ));
+  }
+  function logShout(string) {
+    console.log(string.toUpperCase( ));
+  }
+
+  
+  function  sayHiToHeadphonedRoommate (string) {
+    let hearAns = "I can't hear you!";
+    let indeedAns = "YES INDEED!";
+    let loveAns = "I would love to!";
+    if (string.toLowerCase(string) === string) {
+      return hearAns;
+    }
+    else if (string.toUpperCase(string) === string) {
+      return indeedAns;
+    }
+    else if ("Let's have dinner together!" === string) {
+      return loveAns
+    } }
